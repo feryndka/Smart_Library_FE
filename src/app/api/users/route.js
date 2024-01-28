592cea6b8d0b926bf4c76c198eb0c68adf3467e7
feryndka <feryandika151@gmail.com>
2024-01-28 21:51:07 +0700

    fix v.2

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,12 +1,59 @@
 import { query } from "../../../lib/db";
 
 export async function GET(request) {
-  const user = await query({
-    query: "SELECT * FROM users",
-    values: [],
-  });
+  // const user = await query({
+  //   query: "SELECT * FROM users",
+  //   values: [],
+  // });
 
-  let data = JSON.stringify(user);
+  // let data = JSON.stringify(user);
+  const data = [
+    {
+      name: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      address: '261 Erdman Ford',
+      city: 'East Daphne',
+      state: 'Kentucky',
+    },
+    {
+      name: {
+        firstName: 'Jane',
+        lastName: 'Doe',
+      },
+      address: '769 Dominic Grove',
+      city: 'Columbus',
+      state: 'Ohio',
+    },
+    {
+      name: {
+        firstName: 'Joe',
+        lastName: 'Doe',
+      },
+      address: '566 Brakus Inlet',
+      city: 'South Linda',
+      state: 'West Virginia',
+    },
+    {
+      name: {
+        firstName: 'Kevin',
+        lastName: 'Vandy',
+      },
+      address: '722 Emie Stream',
+      city: 'Lincoln',
+      state: 'Nebraska',
+    },
+    {
+      name: {
+        firstName: 'Joshua',
+        lastName: 'Rolluffs',
+      },
+      address: '32188 Larkin Turnpike',
+      city: 'Charleston',
+      state: 'South Carolina',
+    },
+  ];
   return new Response(data, {
     status: 200,
   });
